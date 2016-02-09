7d938d4cb6d2d164ea460a07458314b60731b4e8
Nima Boscarino <nboscari@sfu.ca>
2016-02-08 16:53:51 -0800

    still working on being able to delete songs

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -24,12 +24,12 @@ function addSong() {
     url: youtube_url,
     success : function(data) {
       var rowCount = $('#p_table tr').length;
-      $( "#p_table" ).append( "<tr><td><button class=song_select type=button onclick=playSong('" + rowCount + "')>" + data.items[0].snippet.title + "</button></tr></td>" );
+      $( "#p_table" ).append( "<tr><td><button id=song_play class=song_select type=button onclick=playSong('" + rowCount + "')>" + data.items[0].snippet.title + "</button><button id=song_delete class=anju_button id=play_all type=button onclick=deleteSong('" + rowCount + "')>DELETE</button></tr></td>" );
       $( "#secret_table" ).append( "<tr><td>" + vid_id + "</tr></td>" );
     }
   });
 
-  document.getElementById("playlist").style.display = "inline-block"
+  document.getElementById("playlist").style.display = "inline-block";
 
 }
 
@@ -52,13 +52,26 @@ function playSong(song_index) {
 
 }
 
-function playAll() {
+function deleteSong(song_index) {
+
+  document.getElementById("p_table").deleteRow(song_index);
+  document.getElementById("secret_table").deleteRow(song_index);
+
+  
+
+  //now I need to iterate through the playlist table and update every playSong() pointer and deleteSong() pointer
+  //I think the secret table should be fine
+  //honestly I'm just going to use a stack to do this. It'll be fine
+
+
+
+  testFunction();
 
 }
 
 function testFunction() {
 
   var tester = document.getElementById("debug_box");
-  tester.innerHTML = parseInt(current_song) + 1;
+  tester.innerHTML = "HELLO";
 
 }
